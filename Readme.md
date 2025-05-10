e65a0e32b1ed7a9a4989fd9313917e450da411fe
SenaCrasher <asrafael255@gmail.com>
2025-05-10 23:59:56 +0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Portfolio-Rafael
+Tanpa Bekas Tertentu
+
+
+
+
+
+
+
+
+
+
+
+
+Hanyalah Seorang Anak Nakal Yang Bercita-cita Untuk Membahagiakan Kedua Orang Tuanya Dan Juga Pasangan Hidupnya Karna Itulah Yang Mwnjadi Kebahagiaan Anak itu Sendiri
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Terjerumus","Kedalam Dunia Malam","Adalah Hal Yang","Paling Ku Sesali","Selama Hidupku","Menjadi Anak Nakal","Bukanlah Keinginanku","Ketenangan","Di Dalam Dunia Itu","Yang Membuatku","Sangan Nyaman","Berada Di Dalamnya"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 70,
+    loop: true
+});
